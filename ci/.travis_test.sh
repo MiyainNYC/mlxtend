4a6cf40e9cfb64a3ae81261fa3f9c1cf1be55360
rasbt <mail@sebastianraschka.com>
2016-04-21 23:01:13 -0400

    restruct travis

--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -3,7 +3,10 @@
 set -e
 
 if [ "$TENSORFLOW" == "true" ]; then
-    nosetests -s -v mlxtend.tf_classifier --nologcapture
+    if [[ "$COVERAGE" == "true" ]]; then
+        nosetests -s -v mlxtend.tf_classifier --nologcapture --with-coverage
+    else
+        nosetests -s -v mlxtend.tf_classifier --nologcapture
 else
     if [[ "$COVERAGE" == "true" ]]; then
         nosetests -s -v --with-coverage --exclude-dir=mlxtend/tf_classifier --exclude-dir=mlxtend/data --exclude-dir=mlxtend/general_plotting
